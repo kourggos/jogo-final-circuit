661c6d32bb1c5daa079019c914889656ad98ed3d
Paulo Rosa <89664967+PauloRosa1@users.noreply.github.com>
2025-01-11 14:18:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,27 +8,35 @@ from character import *
 clock = pygame.time.Clock()
 
 janela = Window(1360, 720)
-cenario = GameImage("background2.png")
-floor = GameImage("floor.png")
-ceiling = GameImage("ceiling.png")
+cenario = GameImage("PPlay/scenary/background2.png")
+floor = GameImage("PPlay/scenary/floor.png")
+ceiling = GameImage("PPlay/scenary/ceiling.png")
 player = Character("playerinv.png", 0, 0, 1, 1)
-teclado = janela.get_keyboard()
 floor.y = janela.height - floor.height
 player.y = floor.y - player.height
-player.x = janela.width/4
+player.x = janela.width / 4
 jumping = False
-maxheight = player.y + 3*player.height/2
 gravity = 1
 jumpheight = 20
 yvel = jumpheight
 
-
 while True:
     clock.tick(60)
-    if teclado.key_pressed("space"):
+
+    if pygame.key.get_pressed()[pygame.K_SPACE] and not jumping:
         jumping = True
     if jumping:
         jumping, gravity, jumpheight, yvel = player.jump(gravity, jumpheight, yvel)
+
+    player.dash(20)
+
+    #reseta quando cai no chao
+    if player.y >= floor.y - player.height:
+        player.y = floor.y - player.height
+        player.touched_ground = True  # jogador ta no chão
+        jumping = False
+        player.can_dash = True  # pra usar o dash dnv
+
     player.move(12)
     cenario.draw()
     floor.draw()
