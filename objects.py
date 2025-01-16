abcd366d47e59c1c0f36b39a3a7d2797ef743eb3
kourggos <guilhermedhfontoura@gmail.com>
2025-01-16 01:05:17 -0300

    Update objects.py

--- objects.py
+++ objects.py
@@ -9,10 +9,10 @@ class Platform():
         self.body.y = y
 
     def move(self, player, background, esq, velx=0, vely=0):
-        if player.image.x >= self.janela.width*7.5/21 and pygame.key.get_pressed()[pygame.K_RIGHT] and not background.bg_change and not esq:
-            self.body.x -= 1.15 # 0.9
-        elif player.image.x >= self.janela.width*7/21 and player.image.x < self.janela.width*7.5/21  and pygame.key.get_pressed()[pygame.K_RIGHT] and not background.bg_change and not esq:
-            self.body.x -= 0.9 # 0.6
+        if player.image.x >= self.janela.width*7.5/21 and pygame.key.get_pressed()[pygame.K_RIGHT] and not background.bg_change and not esq and not player.dashing:
+            self.body.x -= 700*self.janela.delta_time() # 0.9
+        elif player.image.x >= self.janela.width*7/21 and player.image.x < self.janela.width*7.5/21  and pygame.key.get_pressed()[pygame.K_RIGHT] and not background.bg_change and not esq and not player.dashing:
+            self.body.x -= 400*self.janela.delta_time() # 0.6
         if self.body.x + self.body.width < 0:
             return True
         return False
