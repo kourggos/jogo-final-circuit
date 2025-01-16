ab66ecfc2349948dd890c5910681110705a62a3c
Paulo Rosa <89664967+PauloRosa1@users.noreply.github.com>
2025-01-15 23:04:23 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from fade import *
 from movingScenery import *
 from objects import *
 from random import randint
+import subprocess
 
 janela = Window(1360, 720)
 background = Background(janela)
@@ -18,13 +19,14 @@ maxheight = ceiling.ceiling.y + ceiling.ceiling.height
 player = Character("playerrect.png", 0, 0, 1, 1, janela)
 player.image.y = floor.floor.y - player.height
 player.image.x = janela.width / 4
-# -----------
+
+# ----------- configurações de parede e colisões
 p_up_wall = GameImage("playervertwall.png")
 p_up_wall.y = player.image.y
-p_up_wall.x = player.image.x + player.image.width/2
+p_up_wall.x = player.image.x + player.image.width / 2
 p_down_wall = GameImage("playervertwall.png")
 p_down_wall.y = player.image.y + player.image.height - p_down_wall.height
-p_down_wall.x = player.image.x + player.image.width/2
+p_down_wall.x = player.image.x + player.image.width / 2
 p_left_wall = GameImage("playersidewall.png")
 p_left_wall.y = player.image.y
 p_left_wall.x = player.image.x
@@ -32,17 +34,18 @@ p_right_wall = GameImage("playersidewall.png")
 p_right_wall.y = player.image.y
 p_right_wall.x = player.image.x + player.image.width - p_right_wall.width
 
-l = [p_up_wall,p_down_wall,p_left_wall,p_right_wall]
-# -----------
+l = [p_up_wall, p_down_wall, p_left_wall, p_right_wall]
+
+# ----------- plataformas e outras configurações
 all_platforms_list = [("platforms/1x3box.png", minHeight - 448),
-                      ("platforms/1x3box.png", minHeight - 64),   # nome do arquivo e posição Y
-                ("platforms/2x2box.png", minHeight - 128),
-                ("platforms/1x3tube.png", minHeight - 128),
-                ("platforms/3x4hollowbox.png", maxheight),
-                ("platforms/3x4hollowbox.png", minHeight - 192),
-                ("platforms/3x05shallowplatform.png", minHeight - 320),
-                ("platforms/5x1pillar.png", maxheight),
-                ("platforms/3x1tube.png", minHeight - 192)]
+                      ("platforms/1x3box.png", minHeight - 64),
+                      ("platforms/2x2box.png", minHeight - 128),
+                      ("platforms/1x3tube.png", minHeight - 128),
+                      ("platforms/3x4hollowbox.png", maxheight),
+                      ("platforms/3x4hollowbox.png", minHeight - 192),
+                      ("platforms/3x05shallowplatform.png", minHeight - 320),
+                      ("platforms/5x1pillar.png", maxheight),
+                      ("platforms/3x1tube.png", minHeight - 192)]
 game_platforms = []
 jumping = False
 gravity = 9
@@ -51,11 +54,59 @@ yvel = 1600
 frames = 0
 cronometro = 0
 fps = 0
-coli = {"cima":False, "baixo":False, "direita":False, "esquerda":False}
+coli = {"cima": False, "baixo": False, "direita": False, "esquerda": False}
 esq = False
 dire = False
 cima = False
 baixo = False
+
+timer_seconds = 30 #muda aqui o timer do jogo.
+timer_font = pygame.font.Font("fonts/PressStart2P-Regular.ttf", 30)
+pygame.mixer.music.load("PPlay/musics/TRON_legacy.mp3")
+pygame.mixer.music.play(-1)
+timer_running = True
+
+pontos = 0
+
+def format_time(seconds):
+    minutes = seconds // 60
+    seconds = seconds % 60
+    return f"{minutes:02}:{seconds:02}"
+
+import os
+
+def save_to_ranking(player_name, score):
+    try:
+        with open("ranking.txt", "r") as file:
+            rankings = file.readlines()
+    except FileNotFoundError:
+        rankings = []
+
+    rankings.append(f"{player_name} - {score} segundos\n")
+    #ordem decrescente
+    rankings.sort(key=lambda x: int(x.split(" - ")[1].split(" ")[0]), reverse=True)
+    #salvando
+    with open("ranking.txt", "w") as file:
+        file.writelines(rankings)
+
+
+def game_over():
+    global timer_running
+    timer_running = False
+
+    game_over_surface = timer_font.render("VOCÊ PERDEU!", True, (255, 0, 0))
+    janela.get_screen().blit(game_over_surface, (janela.width / 2 - 200, janela.height / 2 - 50))
+
+    janela.update()
+    print("Digite seu nome para salvar no ranking: ")
+    player_name = input().strip()
+    if player_name:
+        save_to_ranking(player_name, pontos)
+
+    pygame.quit()
+    subprocess.run(["python", "menu.py"])
+    exit()  # Sai do jogo
+
 
 while True:
     # ------- fps -------
@@ -65,17 +116,28 @@ while True:
         fps = frames
         frames = 0
         cronometro = 0
+
+        # Atualizando o timer
+        if timer_running:
+            timer_seconds -= 1
+            pontos += 1  #+1 ponto todo segundo
+            if timer_seconds <= 0:
+                result = game_over()
+                if result == "menu":
+                    break
     # --------------------
 
     background.move(player, coli["esquerda"], coli["direita"])
     floor.move(player, background, coli["esquerda"])
     ceiling.move(player, background, coli["esquerda"])
 
+    # ----------- movimento das paredes e player -----------
+
     p_up_wall.y = player.image.y
-    p_up_wall.x = player.image.x + player.image.width/2 - p_up_wall.width/2
+    p_up_wall.x = player.image.x + player.image.width / 2 - p_up_wall.width / 2
 
     p_down_wall.y = player.image.y + player.image.height - p_down_wall.height
-    p_down_wall.x = player.image.x + player.image.width/2 - p_down_wall.width/2
+    p_down_wall.x = player.image.x + player.image.width / 2 - p_down_wall.width / 2
 
     p_left_wall.y = player.image.y
     p_left_wall.x = player.image.x
@@ -84,7 +146,7 @@ while True:
     p_right_wall.x = player.image.x + player.image.width - p_right_wall.width
 
     # ---------------------
-    
+
     if pygame.key.get_pressed()[pygame.K_SPACE] and not jumping and coli["baixo"]:
         jumping = True
         coli["baixo"] = False
@@ -93,40 +155,39 @@ while True:
         jumping, gravity, jumpheight, yvel = player.jump(coli["baixo"], coli["cima"], gravity, jumpheight, yvel)
 
     # ----------- colisões player-plataformas -------------
-    
+
     for platform in game_platforms:
-        if (Collision.collided(player.image, platform.body) and p_right_wall.x < platform.body.x): #and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
+        if (Collision.collided(player.image, platform.body) and p_right_wall.x < platform.body.x):  # and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
             coli["esquerda"] = True
             esq = True
         elif not esq:
             coli["esquerda"] = False
-        
-        if (Collision.collided(player.image, platform.body) and p_left_wall.x + p_left_wall.width > platform.body.x + platform.body.width):  #and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
+
+        if (Collision.collided(player.image, platform.body) and p_left_wall.x + p_left_wall.width > platform.body.x + platform.body.width):  # and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
             coli["direita"] = True
             dire = True
         elif not dire:
             coli["direita"] = False
-    	
-        if (Collision.collided(p_up_wall, platform.body) and p_down_wall.y > platform.body.y + platform.body.height): #and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
+
+        if (Collision.collided(p_up_wall, platform.body) and p_down_wall.y > platform.body.y + platform.body.height):  # and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
             coli["cima"] = True
             cima = True
         elif not cima:
             coli["cima"] = False
 
-        if (Collision.collided(p_down_wall, platform.body) and p_up_wall.y < platform.body.y): #and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
+        if (Collision.collided(p_down_wall, platform.body) and p_up_wall.y < platform.body.y):  # and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
             coli["baixo"] = True
             player.touched_ground = True
             baixo = True
         elif not baixo:
             coli["baixo"] = False
-    
+
     esq = False
     dire = False
     cima = False
-    baixo = False   
- 
+    baixo = False
+
     # movimento das plataformas
-    
     for platform in game_platforms:
         if platform.move(player, background, coli["esquerda"]):
             game_platforms.remove(platform)
@@ -134,11 +195,8 @@ while True:
         else:
             platform.draw()
 
-    #print(coli["esquerda"], coli["direita"], coli["cima"], coli["baixo"])
-
-
-    # ----------- gerar plataformas, precisa melhorar
-    r = randint(0, len(all_platforms_list)-1)
+    # ----------- gerar plataformas
+    r = randint(0, len(all_platforms_list) - 1)
     p = Platform(all_platforms_list[r][0], janela, y=all_platforms_list[r][1])
     for platform in game_platforms:
         if (platform.body.y < p.body.y + p.body.height and platform.body.y + platform.body.height > p.body.y):
@@ -149,24 +207,20 @@ while True:
             game_platforms.append(p)
     except:
         pass
-    # --------------
 
     if not coli["baixo"] and player.image.y + player.image.height < floor.floor.y and not jumping:
-        player.image.y += 1600*janela.delta_time()
+        player.image.y += 1600 * janela.delta_time()
 
     # ------------------------
 
-
-
     player.dash(1400)
-    #reseta quando cai no chao
     if player.image.y >= floor.floor.y - player.image.height:
         player.image.y = floor.floor.y - player.image.height
-        player.touched_ground = True  # jogador ta no chão
+        player.touched_ground = True
         coli["baixo"] = True
         baixo = True
         jumping = False
-        player.can_dash = True  # pra usar o dash dnv
+        player.can_dash = True
     if player.image.y <= ceiling.ceiling.height:
         coli["cima"] = True
         cima = True
@@ -174,5 +228,12 @@ while True:
     player.draw()
     for wall in l:
         wall.draw()
-    janela.draw_text("fps:" + str(fps), 0, 0, size=14, color=(255,255,255))
+
+    #desenhando o temporizador
+    if timer_running:
+        timer_text = format_time(timer_seconds)
+        timer_surface = timer_font.render(timer_text, True, (255, 255, 255))
+        janela.get_screen().blit(timer_surface, (janela.width - 150, 10))
+
+    janela.draw_text("fps:" + str(fps), 0, 0, size=14, color=(255, 255, 255))
     janela.update()
---
+++ menu.py
@@ -0,0 +1,229 @@
+import pygame
+from PPlay.window import Window
+from PPlay.gameimage import GameImage
+from PPlay.mouse import Mouse
+import os
+import subprocess
+
+
+class Menu:
+    def __init__(self, janela):
+        self.janela = janela
+        self.mouse = Mouse()
+        self.font = pygame.font.Font("fonts/PressStart2P-Regular.ttf", 30)
+
+        pygame.mixer.music.load("PPlay/musics/solar_sailer.mp3")
+        pygame.mixer.music.play(-1)
+        self.hover_sound = pygame.mixer.Sound("PPlay/musics/hover.mp3")
+
+        # inicializando os botoes(x, y, largura, altura)
+        self.background = GameImage("scenery/background.png")
+        self.play_button = pygame.Rect(530, 200, 300, 60)
+        self.how_to_play_button = pygame.Rect(530, 280, 300, 60)
+        self.rank_button = pygame.Rect(530, 360, 300, 60)
+        self.exit_button = pygame.Rect(530, 440, 300, 60)
+
+    def draw_button(self, button_rect, text, active=False):
+        # pra quando o mouse passar por cima
+        color = (0, 255, 0) if active else (255, 255, 255)
+        pygame.draw.rect(self.janela.screen, color, button_rect, 2)
+        label = self.font.render(text, True, color)
+        self.janela.screen.blit(label, (button_rect.x + (button_rect.width - label.get_width()) / 2,
+                                        button_rect.y + (button_rect.height - label.get_height()) / 2))
+
+    def show(self):
+        last_played_button = None
+
+        while True:
+            self.janela.update()
+            self.janela.screen.fill((0, 0, 0))
+            self.background.draw()
+
+            mouse_x, mouse_y = self.mouse.get_position()
+
+            #ve se mouse sobre um botao
+            play_active = self.play_button.collidepoint(mouse_x, mouse_y)
+            how_to_play_active = self.how_to_play_button.collidepoint(mouse_x, mouse_y)
+            rank_active = self.rank_button.collidepoint(mouse_x, mouse_y)
+            exit_active = self.exit_button.collidepoint(mouse_x, mouse_y)
+
+            #se o mouse estiver em cima de um botao, toca o som
+            if play_active and last_played_button != "play":
+                self.hover_sound.play()
+                last_played_button = "play"
+            elif how_to_play_active and last_played_button != "how_to_play":
+                self.hover_sound.play()
+                last_played_button = "how_to_play"
+            elif rank_active and last_played_button != "rank":
+                self.hover_sound.play()
+                last_played_button = "rank"
+            elif exit_active and last_played_button != "exit":
+                self.hover_sound.play()
+                last_played_button = "exit"
+            elif not (play_active or how_to_play_active or rank_active or exit_active):
+                last_played_button = None  #reset se n tiver em nenhum botao
+
+            self.draw_button(self.play_button, "JOGAR", play_active)
+            self.draw_button(self.how_to_play_button, "COMO JOGAR", how_to_play_active)
+            self.draw_button(self.rank_button, "RANKING", rank_active)
+            self.draw_button(self.exit_button, "SAIR", exit_active)
+
+            # Se o mouse clicou
+            if play_active and self.mouse.is_button_pressed(1):
+                return "jogar"
+            elif how_to_play_active and self.mouse.is_button_pressed(1):
+                return "como_jogar"
+            elif rank_active and self.mouse.is_button_pressed(1):
+                return "ranking"
+            elif exit_active and self.mouse.is_button_pressed(1):
+                return "sair"
+
+
+class Ranking:
+    def __init__(self, janela):
+        self.janela = janela
+        self.mouse = Mouse()
+        self.font = pygame.font.Font("fonts/PressStart2P-Regular.ttf", 30)
+        self.background = GameImage("scenery/background.png")
+        self.hover_sound = pygame.mixer.Sound("PPlay/musics/hover.mp3")
+
+    def draw_button(self, button_rect, text, active=False):
+        # pra quando o mouse passar por cima
+        color = (0, 255, 0) if active else (255, 255, 255)
+        pygame.draw.rect(self.janela.screen, color, button_rect, 2)
+        label = self.font.render(text, True, color)
+        self.janela.screen.blit(label, (button_rect.x + (button_rect.width - label.get_width()) / 2,
+                                        button_rect.y + (button_rect.height - label.get_height()) / 2))
+
+    def show_ranking(self):
+        last_played_button = None
+
+        #lendo arquivo de ranking
+        try:
+            with open("ranking.txt", "r") as file:
+                rankings = file.readlines()
+        except FileNotFoundError:
+            rankings = []
+
+        #quantos rankings vão ser mostrados
+        top_rankings = rankings[:5]
+
+        #exibir resultado
+        while True:
+            self.janela.update()
+
+            # Desenha o fundo do menu
+            self.background.draw()
+
+            self.background.draw()
+
+            y_offset = 70  #começa a partir dessa posicao y
+
+            for i, rank in enumerate(top_rankings):
+                player_name, score = rank.strip().split(" - ")
+                score = score.replace(" segundos", "")  #tira a parte de "segundos. é só tirar isso da main, mas faço isso depois, tanto faz
+
+                #calculando posição fixa para o nome e pontuação
+                name_x = self.janela.width / 2 - 500  #pos coluna para nome
+                score_x = self.janela.width / 2 + 200  #pos coluna para pontuacao
+
+                #desenhar o nome e a pontuação
+                name_label = self.font.render(f"{i + 1}. {player_name}", True, (255, 255, 255))
+                score_label = self.font.render(f"{score} pontos", True, (255, 255, 255))
+
+                self.janela.screen.blit(name_label, (name_x, y_offset))
+                self.janela.screen.blit(score_label, (score_x, y_offset))
+
+                y_offset += 100  #espaçamento entre os rankings
+
+            # Botão de voltar para o menu
+            mouse_x, mouse_y = self.mouse.get_position()
+            back_button = pygame.Rect(self.janela.width - 350, self.janela.height - 100, 200, 50)
+            back_active = back_button.collidepoint(mouse_x, mouse_y)
+
+
+            if back_active and last_played_button != "back":
+                self.hover_sound.play()
+                last_played_button = "back"
+            elif not back_active:
+                last_played_button = None
+
+
+            self.draw_button(back_button, "Voltar", back_active)
+
+            if back_active and self.mouse.is_button_pressed(1):
+                return  # Volta para o menu
+
+
+class HowToPlay:
+
+    def __init__(self, janela):
+        self.janela = janela
+        self.mouse = Mouse()
+        self.font = pygame.font.Font("fonts/PressStart2P-Regular.ttf", 30)
+        self.instructions_image = GameImage("scenery/comojogar.png")
+        self.back_button = pygame.Rect(self.janela.width - 350, self.janela.height - 100, 200, 50)
+        self.hover_sound = pygame.mixer.Sound("PPlay/musics/hover.mp3")
+
+
+    def draw_button(self, button_rect, text, active=False):
+        # se o mouse ta em cima
+        color = (0, 255, 0) if active else (255, 255, 255)
+        pygame.draw.rect(self.janela.screen, color, button_rect, 2)
+        label = self.font.render(text, True, color)
+        self.janela.screen.blit(label, (button_rect.x + (button_rect.width - label.get_width()) / 2,
+                                        button_rect.y + (button_rect.height - label.get_height()) / 2))
+
+    def show(self):
+        last_played_button = None
+
+        while True:
+            self.janela.update()
+            self.janela.screen.fill((0, 0, 0))  #limpa tela
+            self.instructions_image.draw()  #mostra as instrucoes
+
+            mouse_x, mouse_y = self.mouse.get_position()
+
+            back_active = self.back_button.collidepoint(mouse_x, mouse_y)
+
+            if back_active and last_played_button != "back":
+                self.hover_sound.play()
+                last_played_button = "back"
+            elif not back_active:
+                last_played_button = None
+
+            self.draw_button(self.back_button, "Voltar", back_active)
+
+            if back_active and self.mouse.is_button_pressed(1):
+                return "menu"
+
+
+def run_game():
+    import main  # Importa o script main diretamente
+    main.main()  # Chama a função main() do main.py
+    exit()
+
+
+def main():
+    janela = Window(1360, 720)
+    menu = Menu(janela)
+
+    while True:
+        option = menu.show()
+
+        if option == "jogar":
+            run_game()
+            break
+        elif option == "como_jogar":
+            how_to_play = HowToPlay(janela)
+            how_to_play.show()
+        elif option == "ranking":
+            ranking = Ranking(janela)  #instancia nova de ranking
+            ranking.show_ranking()
+        elif option == "sair":
+            janela.close()
+            break
+
+
+if __name__ == "__main__":
+    main()
