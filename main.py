161d78459f297702f9cf655ad777ea11f51a050f
kourggos <guilhermedhfontoura@gmail.com>
2025-01-16 01:04:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,14 +19,13 @@ maxheight = ceiling.ceiling.y + ceiling.ceiling.height
 player = Character("playerrect.png", 0, 0, 1, 1, janela)
 player.image.y = floor.floor.y - player.height
 player.image.x = janela.width / 4
-
-# ----------- configurações de parede e colisões
+# -----------
 p_up_wall = GameImage("playervertwall.png")
 p_up_wall.y = player.image.y
-p_up_wall.x = player.image.x + player.image.width / 2
+p_up_wall.x = player.image.x + player.image.width/2
 p_down_wall = GameImage("playervertwall.png")
 p_down_wall.y = player.image.y + player.image.height - p_down_wall.height
-p_down_wall.x = player.image.x + player.image.width / 2
+p_down_wall.x = player.image.x + player.image.width/2
 p_left_wall = GameImage("playersidewall.png")
 p_left_wall.y = player.image.y
 p_left_wall.x = player.image.x
@@ -34,35 +33,37 @@ p_right_wall = GameImage("playersidewall.png")
 p_right_wall.y = player.image.y
 p_right_wall.x = player.image.x + player.image.width - p_right_wall.width
 
-l = [p_up_wall, p_down_wall, p_left_wall, p_right_wall]
-
-# ----------- plataformas e outras configurações
+l = [p_up_wall,p_down_wall,p_left_wall,p_right_wall]
+# -----------
 all_platforms_list = [("platforms/1x3box.png", minHeight - 448),
-                      ("platforms/1x3box.png", minHeight - 64),
-                      ("platforms/2x2box.png", minHeight - 128),
-                      ("platforms/1x3tube.png", minHeight - 128),
-                      ("platforms/3x4hollowbox.png", maxheight),
-                      ("platforms/3x4hollowbox.png", minHeight - 192),
-                      ("platforms/3x05shallowplatform.png", minHeight - 320),
-                      ("platforms/5x1pillar.png", maxheight),
-                      ("platforms/3x1tube.png", minHeight - 192)]
+                      ("platforms/1x3box.png", minHeight - 64),   # nome do arquivo e posição Y
+                ("platforms/2x2box.png", minHeight - 128),
+                ("platforms/1x3tube.png", minHeight - 128),
+                ("platforms/3x4hollowbox.png", maxheight),
+                ("platforms/3x4hollowbox.png", minHeight - 192),
+                ("platforms/3x05shallowplatform.png", minHeight - 320),
+                ("platforms/5x1pillar.png", maxheight),
+                ("platforms/3x1tube.png", minHeight - 192)]
 game_platforms = []
 jumping = False
-gravity = 9
+gravity = 11
 jumpheight = 1600
 yvel = 1600
 frames = 0
 cronometro = 0
 fps = 0
-coli = {"cima": False, "baixo": False, "direita": False, "esquerda": False}
+coli = {"cima":False, "baixo":False, "direita":False, "esquerda":False}
 esq = False
 dire = False
 cima = False
 baixo = False
+cddash = 0.3
+dictdash = {"left":False, "up":False, "down":False, "right":False}
 
 timer_seconds = 30 #muda aqui o timer do jogo.
 timer_font = pygame.font.Font("fonts/PressStart2P-Regular.ttf", 30)
-pygame.mixer.music.load("PPlay/musics/TRON_legacy.mp3")
+pygame.mixer.music.load("songs/TRON_legacy.mp3")
+pygame.mixer.music.set_volume(0.2)
 pygame.mixer.music.play(-1)
 timer_running = True
 
@@ -88,7 +89,6 @@ def save_to_ranking(player_name, score):
     #salvando
     with open("ranking.txt", "w") as file:
         file.writelines(rankings)
-
 
 def game_over():
     global timer_running
@@ -131,13 +131,11 @@ while True:
     floor.move(player, background, coli["esquerda"])
     ceiling.move(player, background, coli["esquerda"])
 
-    # ----------- movimento das paredes e player -----------
-
     p_up_wall.y = player.image.y
-    p_up_wall.x = player.image.x + player.image.width / 2 - p_up_wall.width / 2
+    p_up_wall.x = player.image.x + player.image.width/2 - p_up_wall.width/2
 
     p_down_wall.y = player.image.y + player.image.height - p_down_wall.height
-    p_down_wall.x = player.image.x + player.image.width / 2 - p_down_wall.width / 2
+    p_down_wall.x = player.image.x + player.image.width/2 - p_down_wall.width/2
 
     p_left_wall.y = player.image.y
     p_left_wall.x = player.image.x
@@ -146,6 +144,7 @@ while True:
     p_right_wall.x = player.image.x + player.image.width - p_right_wall.width
 
     # ---------------------
+    
 
     if pygame.key.get_pressed()[pygame.K_SPACE] and not jumping and coli["baixo"]:
         jumping = True
@@ -155,39 +154,40 @@ while True:
         jumping, gravity, jumpheight, yvel = player.jump(coli["baixo"], coli["cima"], gravity, jumpheight, yvel)
 
     # ----------- colisões player-plataformas -------------
-
+    
     for platform in game_platforms:
-        if (Collision.collided(player.image, platform.body) and p_right_wall.x < platform.body.x):  # and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
+        if (Collision.collided(player.image, platform.body) and p_right_wall.x < platform.body.x): #and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
             coli["esquerda"] = True
             esq = True
         elif not esq:
             coli["esquerda"] = False
-
-        if (Collision.collided(player.image, platform.body) and p_left_wall.x + p_left_wall.width > platform.body.x + platform.body.width):  # and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
+        
+        if (Collision.collided(player.image, platform.body) and p_left_wall.x + p_left_wall.width > platform.body.x + platform.body.width):  #and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
             coli["direita"] = True
             dire = True
         elif not dire:
             coli["direita"] = False
-
-        if (Collision.collided(p_up_wall, platform.body) and p_down_wall.y > platform.body.y + platform.body.height):  # and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
+    	
+        if (Collision.collided(p_up_wall, platform.body) and p_down_wall.y > platform.body.y + platform.body.height): #and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
             coli["cima"] = True
             cima = True
         elif not cima:
             coli["cima"] = False
 
-        if (Collision.collided(p_down_wall, platform.body) and p_up_wall.y < platform.body.y):  # and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
+        if (Collision.collided(p_down_wall, platform.body) and p_up_wall.y < platform.body.y): #and (player.y + player.height >= platform.body.y or player.y < platform.body.y + platform.body.height):
             coli["baixo"] = True
             player.touched_ground = True
             baixo = True
         elif not baixo:
             coli["baixo"] = False
-
+    
     esq = False
     dire = False
     cima = False
-    baixo = False
-
+    baixo = False   
+ 
     # movimento das plataformas
+    
     for platform in game_platforms:
         if platform.move(player, background, coli["esquerda"]):
             game_platforms.remove(platform)
@@ -195,8 +195,11 @@ while True:
         else:
             platform.draw()
 
-    # ----------- gerar plataformas
-    r = randint(0, len(all_platforms_list) - 1)
+    #print(coli["esquerda"], coli["direita"], coli["cima"], coli["baixo"])
+
+
+    # ----------- gerar plataformas, precisa melhorar
+    r = randint(0, len(all_platforms_list)-1)
     p = Platform(all_platforms_list[r][0], janela, y=all_platforms_list[r][1])
     for platform in game_platforms:
         if (platform.body.y < p.body.y + p.body.height and platform.body.y + platform.body.height > p.body.y):
@@ -207,25 +210,56 @@ while True:
             game_platforms.append(p)
     except:
         pass
+    # --------------
 
     if not coli["baixo"] and player.image.y + player.image.height < floor.floor.y and not jumping:
-        player.image.y += 1600 * janela.delta_time()
+        player.image.y += 1600*janela.delta_time()
 
     # ------------------------
 
-    player.dash(1400)
+    dictdash["left"] = pygame.key.get_pressed()[pygame.K_LEFT]
+    dictdash["up"] = pygame.key.get_pressed()[pygame.K_UP]
+    dictdash["down"] = pygame.key.get_pressed()[pygame.K_DOWN]
+    dictdash["right"] = pygame.key.get_pressed()[pygame.K_RIGHT]
+
+    if dictdash["left"] and dictdash["right"]:
+        dictdash["left"] = False
+        dictdash["right"] = False
+    if dictdash["up"] and dictdash["down"]:
+        dictdash["up"] = False
+        dictdash["down"] = False
+
+    if dictdash["down"]:
+        if player.image.y + player.image.height + player.dashsprite_down.height >= minHeight:
+            dictdash["down"] = False
+    if dictdash["up"]:
+        if player.image.y - player.dashsprite_up.height <= maxheight:
+            dictdash["up"] = "ceil"
+
+    if cddash > 1:
+        if pygame.key.get_pressed()[pygame.K_LSHIFT] or player.dashing:
+            player.dashing = True
+            player.dash(dictdash)
+            if not player.dashing:
+                cddash = 0
+    else:
+        cddash += janela.delta_time()
+
+    #player.dash(1400)
+    #reseta quando cai no chao
     if player.image.y >= floor.floor.y - player.image.height:
         player.image.y = floor.floor.y - player.image.height
-        player.touched_ground = True
+        player.touched_ground = True  # jogador ta no chão
         coli["baixo"] = True
         baixo = True
         jumping = False
-        player.can_dash = True
+        player.can_dash = True  # pra usar o dash dnv
     if player.image.y <= ceiling.ceiling.height:
         coli["cima"] = True
         cima = True
 
-    player.draw()
+    if not player.dashing:
+        player.draw()
     for wall in l:
         wall.draw()
 
@@ -235,5 +269,5 @@ while True:
         timer_surface = timer_font.render(timer_text, True, (255, 255, 255))
         janela.get_screen().blit(timer_surface, (janela.width - 150, 10))
 
-    janela.draw_text("fps:" + str(fps), 0, 0, size=14, color=(255, 255, 255))
+    janela.draw_text("fps:" + str(fps), 0, 0, size=14, color=(255,255,255))
     janela.update()
